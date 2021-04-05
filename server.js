6b25ffa9ef40fe2e9f3114b4ca6931acef90897c
Rima <rimameih@gmail.com>
2021-04-05 13:49:08 +0700

    Adding Cors and Body-Parser

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const productRoutes = require("./routes/products");
 const app = express();
+const cors = require("cors");
 
-app.use("/", productRoutes);
+// Adding CORS
+// app.use((req, res, next) => {
+//   res.setHeader("Acces-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Acces-Control-Allow-Methods",
+//     "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+//   );
+//   res.setHeader("Acces-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
+
+app.use(cors());
+
+app.use("/v1/customer", productRoutes);
 
 app.listen(4000);
