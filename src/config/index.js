7b99dc88d745dcc860a67d7af79f99211d488453
Rima <rimameih@gmail.com>
2024-07-23 00:05:15 +0700

    fix: remove unused

--- src/config/index.js
+++ src/config/index.js
@@ -6,7 +6,6 @@ dotenv.config();
 export const config = {
   PORT: process.env.PORT || 4000,
   DATABASE_URI: process.env.DATABASE_URI || 'mongodb://localhost:27017/test',
-  JWT_ACTIVATION_SECRET: process.env.JWT_ACTIVATION_SECRET,
   JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
   JWT_EXPIRES_IN: '1d',
 };
